af9954964ef83b56dddc6886cb599851a439af33
Gopinath Puli Desikan <gopinath.desikan@in.verizon.com>
2024-01-18 13:37:07 +0530

    Day 23 exercise completed

--- 23-turtle-crossing/main.py
+++ 23-turtle-crossing/main.py
@@ -8,7 +8,39 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+# Create a turtle player
+player = Player()
+
+# Listen to the screen
+screen.listen()
+screen.onkey(player.move, "Up")
+
+# Initialize the Car Manager
+cars_manage = CarManager()
+
+# Initialize the scoreboard
+scoreboard = Scoreboard()
+
 game_is_on = True
+
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    cars_manage.create_cars()
+    cars_manage.move_cars()
+
+    # Detect when the player collides with a car
+    for car in cars_manage.all_cars:  # run through all the segments except head (index 0)
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.stop_game()
+
+    # Detect when the player touches the top wall
+    if player.is_finish_line():
+        cars_manage.increase_speed()
+        scoreboard.level_up()
+        player.reset_position()
+
+
+screen.exitonclick()
--- 23-turtle-crossing/scoreboard.py
+++ 23-turtle-crossing/scoreboard.py
@@ -1,5 +1,26 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.level = 1
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-220, 260)
+        self.write(f"Level: {self.level}", align="center", font=FONT)
+
+    def level_up(self):
+        self.level += 1
+        self.update_scoreboard()
+
+    def stop_game(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=FONT)
+
